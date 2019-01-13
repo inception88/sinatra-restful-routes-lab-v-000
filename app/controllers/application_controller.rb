a0c7ab8356421be490a582a988cf4b628e5cf009
Benjamin Jones <bljones36@gmail.com>
2019-01-13 19:18:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
   post '/recipes' do
     recipe = Recipe.new(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     recipe.save
-    redirect "/recipes"
+    # redirect "/recipes"
   end
 
   get '/recipes/:id' do
