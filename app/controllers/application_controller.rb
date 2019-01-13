57fac813288ee0d7e387981c6e65950d6fb04a9c
Benjamin Jones <bljones36@gmail.com>
2019-01-13 19:13:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/new' do
-    
+    recipe = Recipe.new(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
   end
 
   get '/recipes/:id' do
